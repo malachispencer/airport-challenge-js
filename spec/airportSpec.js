b83d90e316a9dfb65e810cd2e840bd010dd28a8c
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-01 20:16:45 +0000

    #takeOff throws exception if weather is stormy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,4 +1,4 @@
-const { Airport } = require('../src/airport.js')
+const { Airport } = require('../src/airport.js');
 
 describe('Airport', () => {
   let airport;
@@ -33,7 +33,7 @@ describe('Airport', () => {
       expect(plane.landed).toHaveBeenCalled();
     });
 
-    it('throws an error if Airport is at max capacity', () => {
+    it('throws an exception if Airport is at max capacity', () => {
       spyOn(airport, 'stormy').and.returnValue(false);
 
       for (let i = 1; i <= 50; i++) {
@@ -43,7 +43,7 @@ describe('Airport', () => {
       expect(function() { airport.land(plane) }).toThrow('Cannot land, Airport at max capacity.');
     });
 
-    it('throws an error if the weather is stormy', () => {
+    it('throws an exception if the weather is stormy', () => {
       spyOn(airport, 'stormy').and.returnValue(true);
       expect(function() { airport.land(plane) }).toThrow('Weather conditions unacceptable for landing.');
     });
@@ -51,27 +51,31 @@ describe('Airport', () => {
 
   describe('#takeOff', () => {
     it('removes a Plane from the plane array', () => {
+      spyOn(airport, 'stormy').and.returnValue(false);
       airport.land(plane);
       airport.takeOff();
       expect(airport.planes.length).toBe(0);
     });
 
     it('returns the removed Plane', () => {
+      spyOn(airport, 'stormy').and.returnValue(false);
       airport.land(plane);
       expect(airport.takeOff()).toEqual(plane);
     });
 
     it('calls the #flying method in the Plane class', () => {
+      spyOn(airport, 'stormy').and.returnValue(false);
       airport.land(plane);
       airport.takeOff();
       expect(plane.flying).toHaveBeenCalled();
     });
 
-    it('throws an error if the planes array is empty', () => {
+    it('throws an exception if the planes array is empty', () => {
+      spyOn(airport, 'stormy').and.returnValue(false);
       expect(function() { airport.takeOff() }).toThrow('Airport empty.')
     });
 
-    it('throws an error if the weather is stormy', () => {
+    it('throws an exception if the weather is stormy', () => {
       spyOn(airport, 'stormy').and.returnValue(false);
       airport.land(plane);
       airport.stormy = jasmine.createSpy('stormy').and.returnValue(true);
